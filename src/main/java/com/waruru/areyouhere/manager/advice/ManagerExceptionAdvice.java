20172c2b969199b2d315bac8d33ef0c1b22aef6e
SHEOMM <esh0821@snu.ac.kr>
2024-02-16 13:51:37 +0900

    refactor: Manager 이름 변경에 따른 모든 layer 명명 변경 및 오류 수정

--- src/main/java/com/waruru/areyouhere/manager/advice/ManagerExceptionAdvice.java
+++ src/main/java/com/waruru/areyouhere/manager/advice/ManagerExceptionAdvice.java
@@ -1,20 +1,21 @@
-package com.waruru.areyouhere.user.advice;
+package com.waruru.areyouhere.manager.advice;
 
 
 import static com.waruru.areyouhere.common.utils.HttpStatusResponseEntity.RESPONSE_CONFLICT;
 import static com.waruru.areyouhere.common.utils.HttpStatusResponseEntity.RESPONSE_FORBIDDEN;
 import static com.waruru.areyouhere.common.utils.HttpStatusResponseEntity.RESPONSE_NOT_FOUND;
 
-import com.waruru.areyouhere.user.exception.DuplicatedEmailException;
-import com.waruru.areyouhere.user.exception.MemberNotFoundException;
-import com.waruru.areyouhere.user.exception.UnAuthenticatedException;
+import com.waruru.areyouhere.manager.exception.DuplicatedEmailException;
+import com.waruru.areyouhere.manager.exception.MemberNotFoundException;
+import com.waruru.areyouhere.manager.exception.UnAuthenticatedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.waruru.areyouhere.user")
-public class UserExceptionAdvice {
+public class ManagerExceptionAdvice {
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<HttpStatus> memberNotFoundException() {
         return RESPONSE_NOT_FOUND;
@@ -28,6 +29,11 @@ public class UserExceptionAdvice {
     @ExceptionHandler(DuplicatedEmailException.class)
     public ResponseEntity<HttpStatus> duplicatedEmailException(){
         return RESPONSE_CONFLICT;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<HttpStatus> methodArgumentNotValidException(){
+        return RESPONSE_FORBIDDEN;
     }
 
 }
